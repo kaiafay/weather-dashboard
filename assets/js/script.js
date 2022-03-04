1def4941204ea5bceab684bf6633850a25986c71
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-03 16:44:01 -0800

    pull elements from HTML into js file

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// pull HTML elements using the DOM
+var cityInput = document.getElementById("city");
+var searchBtn = document.getElementById("submit-search");
+var currentDayContainer = document.getElementById("current-day");
+var searchHistoryContainer = document.getElementById("search-history");
+var forecastContainer = document.getElementById("forecast");
