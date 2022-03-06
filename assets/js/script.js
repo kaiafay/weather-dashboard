e72fcacd0d8767f41d5da9410441fd24db97b674
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-06 10:57:41 -0800

    create global elements for API and search history

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,8 @@ var searchBtn = document.getElementById("submit-search");
 var currentDayContainer = document.getElementById("current-day");
 var searchHistoryContainer = document.getElementById("search-history");
 var forecastContainer = document.getElementById("forecast");
+
+// global variables
+var apiURL = 'http://api.openweathermap.org/'; 
+var apiKey = '924c79f0f636f17fd8929bb3a3510184';
+var searchHistory = [];
