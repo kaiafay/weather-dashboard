9fba6a2c36a5cb7a46a32556137a6a1cfadecc70
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-06 11:17:28 -0800

    add function that will display search history on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,4 +8,9 @@ var forecastContainer = document.getElementById("forecast");
 // global variables
 var apiURL = 'http://api.openweathermap.org/'; 
 var apiKey = '924c79f0f636f17fd8929bb3a3510184';
-var searchHistory = [];
+var searchHistory = ['LA', 'New Zealand', 'New Mexico'];
+
+// display the search history on the page
+var displayHistory = function() {
+    searchHistoryContainer.innerHTML = '';
+};
