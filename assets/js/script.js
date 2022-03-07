c00886a1218de15fa01d7404d0c5968c8baaebc6
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-07 12:29:57 -0800

    create function for current day weather and define variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,41 @@ var searchHistory = [];
 var displayHistory = function() {
     searchHistoryContainer.innerHTML = '';
 };
+
+var getCurrentDay = function(city) {
+    // set city variable to city input value
+    city = $("#city").val();
+
+    // set variable for current day API URL
+    var currentDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+    fetch(currentDayURL).then(function(response) {
+        // display data from API
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+        // create variables for data
+        var temp = data.main.temp;
+        // convert temp to fahrenheit
+        var tempF = Math.round((temp - 273.15) * 1.8 + 32);
+        var wind = data.wind.speed;
+        var humidity = data.main.humidity;
+        var icon = data.weather[0].icon;
+        var cityName = data.name;
+        var date = moment().format("MMMM Do YYYY");
+        // not working as expected
+        var tempEl = $("li").text("Temp:" + tempF + " F");
+        console.log(tempEl);
+        // append temp to current day container
+        currentDayContainer.append(tempEl);
+        currentDayContainer.append(date);
+    });
+
+};
+
+// add event listener to the search button
+searchBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+
+    // call the function to get current day weather stats
+    getCurrentDay();
+})
